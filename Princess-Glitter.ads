e6145990539bdbec76b31355d8981bf7d8c40f92
chaimalk <108950943+chaimalk@users.noreply.github.com>
2023-02-17 22:47:43 +0100

    Create Princess-Glitter.ads

---
+++ Princess-Glitter.ads
@@ -0,0 +1,14 @@
+{
+  "AdsController": {
+    "adNetwork": "unity",
+    "admobBanner": "ca-app-pub-3940256099942544/6300978111",
+    "admobInterstitial": "ca-app-pub-3940256099942544/1033173712",
+    "applovinBanner": "2dffb0bdab9918e3",
+    "applovinInterstitial": "dd1e23e8ec4f80bf",
+    "unityGameId": "5169669",
+    "testMode": true,
+    "unityBanner": "Banner_Android",
+    "unityInterstitial": "Interstitial_Android",
+    "reviewActive": true
+  }
+}
